bd28ca16938ea0a115d56dbf81a1b1a7f534b476
ChyBeat <chybeat@gmail.com>
2021-03-07 00:32:39 -0500

    Sublime Text data finished. Added SASS developer files

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+        css_dir = "css"
+        sass_dir = "dev/sass"
+        images_dir = "assets"
+        javascripts_dir = "javascripts"
+        output_style = :compressed
+        relative_assets=true
+        line_comments = false
+        
