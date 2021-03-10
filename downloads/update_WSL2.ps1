306e352942319cd3873974174dd47e057be19b59
ChyBeat <chybeat@gmail.com>
2021-03-10 06:40:37 -0500

    Corrected some text issues and better explained .ps1

--- downloads/update_WSL2.ps1
+++ downloads/update_WSL2.ps1
@@ -1,15 +1,24 @@
-﻿#variable de descarga de ubuntu desde microsoft, si existe una uri mas actual, se puede actualizar
-$uriForLinux = "https://aka.ms/wslubuntu2004"
-
+﻿#Verifying the run as admin status
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){
   # Relaunch as an elevated process:
   Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
   exit
 }
+clear-host;
+
+#Carpeta donde se descargaran y posteiormente se eliminaran los archivos descargados
 $DesktopPath = [Environment]::GetFolderPath("Desktop")
 
-cls;
-function Show-txt($txt){
+#variable de descarga de ubuntu desde microsoft, si existe una uri mas actual, se puede actualizar
+#variable que debe ser adquirida desde la base de datos donde dice el enlace de descarga para cada distro de Linux
+$uriForLinux = "https://aka.ms/wslubuntu2004"
+
+
+function Show-txt{
+    #Funcion que explica el proceso en curso.
+    param(
+        $txt
+    )
     Write-Host -ForegroundColor Yellow -BackgroundColor Black $txt
 }
 
@@ -20,15 +29,14 @@ if($DesktopPath -ne $PSScriptroot){
 $restarted = Get-ItemPropertyValue -LiteralPath "Registry::HKEY_CURRENT_USER\SOFTWARE\wsl2" -Name "Restarted"  -ErrorAction SilentlyContinue
 
 if(!$restarted){
-    New-Item -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\wsl2" -force | out-null
+    New-Item -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\wsl2" -force | Out-Null
     Set-ItemProperty -LiteralPath "Registry::HKEY_CURRENT_USER\SOFTWARE\wsl2" -force -name "Restarted" -value "0" -type "string" -ErrorAction SilentlyContinue
     $restarted = 0;
 }
 
 if($restarted -eq "0"){
     #activación de WSL2 en Windows
-    Show-txt("`nActivando Windows Susbsystem for linux");
-
+    Show-txt("`nActivando Windows Subsystem for linux");
 
     dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart;
 
@@ -47,43 +55,44 @@ if($restarted -eq "0"){
     #ectivacuión del paso 2 del script en el registro
     Set-ItemProperty -LiteralPath "Registry::HKEY_CURRENT_USER\SOFTWARE\wsl2" -force -name "Restarted" -value "1" -type "string" -ErrorAction SilentlyContinue
 
-    #Ejecución automática del escript en el reinicio del computador
+    #Ejecución automática del script luego del reinicio del computador
     $scriptFile = $MyInvocation.Mycommand.Source;
     Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run" -Name "RunWSL2" -Value "powershell.exe -executionpolicy bypass -file `"$scriptFile`"" -Type ExpandString | Out-Null
-    Show-txt("Comenzando el proceso de reinicio. Espera...")
+    Show-txt("Comenzando el proceso de reinicio. Espera...");
 
     #ejecución del reinicio
     Start-Sleep -S 2
     Restart-Computer -force
     exit
 }else{
-    Show-txt("`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`nContinación de activación de WSL2");
+    Show-txt("`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`nContinación de la activación de WSL2");
     Show-txt("Este script terminará automáticamente cuando las dependicas y archivos de WSL2 hayan sido terminadas y removidas");
+
     #actualización del kernel de WSL
     if(!(Get-Item -Path "$DesktopPath\wsl_update_x64.msi" -ErrorAction SilentlyContinue)){
         Show-txt("Descargando la actualización del Kernel de WSL v2...");
         Invoke-WebRequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile $DesktopPath\wsl_update_x64.msi -UseBasicParsing;
     }
 
-    Show-txt("Instalando el subsistema en la version 2");
+    Show-txt("Instalando el Kernel de WSL v2");
     msiexec /i "$DesktopPath\wsl_update_x64.msi" /passive
 
     #activación de WSL2 en maquina virtual   
     Show-txt("Activando WSL2 como versión predeterminada");
     wsl --set-default-version 2 | Out-Null;
 
-    #descarga y activación de Windows
+    #descarga y activación de Linux
     Show-txt("Activando Linux ($uriForLinux)");
     if(!(Get-Item -Path $DesktopPath\linux.appx -ErrorAction SilentlyContinue)){
         Show-txt("Descargando: $uriForLinux (~450.000.000 Bytes)");
         Invoke-WebRequest -Uri $uriForLinux -OutFile $DesktopPath\linux.appx -UseBasicParsing;
     }
-    Add-AppxPackage "$DesktopPath\Linux.appx"
+    Add-AppxPackage "$DesktopPath\linux.appx"
     start-sleep 5
     Show-txt("Terminado...");
     #limpieza de archivos y registro
-    Remove-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run" -Name "RunWSL2"
-    Remove-Item -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\wsl2" -force | out-null
+    Remove-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run" -Name "RunWSL2" - force | Out-Null
+    Remove-Item -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\wsl2" -force | Out-Null
     if((Get-Item -Path "$DesktopPath\wsl_update_x64.msi" -ErrorAction SilentlyContinue)){
        Remove-Item "$DesktopPath\wsl_update_x64.msi"
     }
@@ -91,4 +100,4 @@ if($restarted -eq "0"){
     while(Get-Item -Path "$DesktopPath\linux.appx" -ErrorAction SilentlyContinue){
        Remove-Item "$DesktopPath\linux.appx" -Force
     }
-}
+}# end if second Download
